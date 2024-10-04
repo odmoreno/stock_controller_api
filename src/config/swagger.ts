543005acf35d8793c04c05967d67c1122da286ad
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-03 21:28:16 -0500

    swagger conf

---
+++ src/config/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options: swaggerJSDoc.Options = {
+    swaggerDefinition: {
+        openapi: '3.0.2',
+        tags: [
+            {
+                name: 'Products',
+                description: 'API operations related to products'
+            }
+        ],
+        info: {
+            title: 'REST API node.js / Express/ Typescript',
+            version: "1.0.0",
+            description: "API Docs for Products"
+        }
+    },
+    apis: ['./src/router.ts']
+}
+
+const swaggerSpect = swaggerJSDoc(options)
+
+export default swaggerSpect
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,5 @@
 import { exit } from 'node:process'
-import db from '../config/dbt'
+import db from '../config/db'
 
 const clearDB = async () => {
     try {
