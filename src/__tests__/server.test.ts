7a3a38bbfdffc0ef66e8139f3e9994766c76cbf0
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-03 20:59:30 -0500

    correction de server.test

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -24,7 +24,7 @@ describe('connectDB', () => {
             .mockRejectedValueOnce(new Error('Hubo un error al conectar a la BD'))
         const consoleSpy = jest.spyOn(console, 'log')
 
-        await connectDB
+        await connectDB()
 
         expect(consoleSpy).toHaveBeenCalledWith(
             expect.stringContaining('Hubo un error al conectar a la BD')
