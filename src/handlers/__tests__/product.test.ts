7ab26ddc1eaa959b1d7c54c98206d8a23286795d
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-03 20:04:09 -0500

    delete test

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -173,6 +173,31 @@ describe('PUT /api/products/:id', () => {
         expect(response.status).not.toBe(400)
         expect(response.body).not.toHaveProperty('errors')
     })
+})
 
 
+describe('DELETE /api/products/:id', () => {
+    it('should check a valid ID', async () => {
+        const response = await request(server).delete('/api/products/not-valid')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('should return a 404 response for a non-existent product', async () => {
+        const productId = 2000
+        const response = await request(server).delete(`/api/products/${productId}`)
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Producto No Encontrado')
+        expect(response.status).not.toBe(200)
+    })
+
+    it('should delete a product', async () => {
+        const response = await request(server).delete('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBe("Producto Eliminado")
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(400)
+    })
 })
